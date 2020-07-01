18c0ea3c91f997d6b7a4146c511ea33c081c58cd
Karol Krzosa <krzosa.karol@gmail.com>
2020-07-01 09:38:44 +0200

    Update, move send response to routes

--- acapy_plugin/routes.py
+++ acapy_plugin/routes.py
@@ -19,10 +19,6 @@ class SendSchema(Schema):
     connection_id = fields.Str(required=True)
 
 
-class GetSchema(Schema):
-    hashid = fields.Str(required=True)
-
-
 @docs(tags=["Schema Exchange"], summary="Send a schema to the connection")
 @request_schema(SendSchema())
 async def send(request: web.BaseRequest):
@@ -50,6 +46,50 @@ async def send(request: web.BaseRequest):
     )
 
 
+class SendSchemaResponse(Schema):
+    payload = fields.Str(required=True)
+    hashid = fields.Str(required=True)
+    # TODO: decision
+    state = fields.Str(required=True)
+    connection_id = fields.Str(required=True)
+
+
+@docs(tags=["Schema Exchange"], summary="Send response to the pending schema request")
+@request_schema(SendSchemaResponse())
+async def sendResponse(request: web.BaseRequest):
+    context = request.app["request_context"]
+    outbound_handler = request.app["outbound_message_router"]
+    params = await request.json()
+    logger = logging.getLogger(__name__)
+    logger.debug(
+        "ROUTES SCHEMA EXCHANGE SEND RESPONSE \nconnection_id:%s \nstate:%s \nhashid: %s \npayload: %s",
+        params["connection_id"],
+        params["state"],
+        params["hashid"],
+        params["payload"],
+    )
+
+    try:
+        connection: ConnectionRecord = await ConnectionRecord.retrieve_by_id(
+            context, params["connection_id"]
+        )
+    except StorageNotFoundError:
+        raise web.HTTPNotFound()
+
+    if connection.is_ready:
+        message = SchemaExchange(payload=params["payload"])
+        await outbound_handler(message, connection_id=connection.connection_id)
+
+    return web.json_response(
+        {
+            "payload": message.payload,
+            "connection_id": params["connection_id"],
+            "state": params["state"],
+            "hashid": params["hashid"],
+        }
+    )
+
+
 @docs(tags=["Schema Exchange"], summary="Get schema by schema id")
 async def get(request: web.BaseRequest):
     context = request.app["request_context"]
@@ -66,7 +106,7 @@ async def get(request: web.BaseRequest):
 
     return web.json_response(
         {
-            "hashid": record.hashid,
+            "hashid": params["hashid"],
             "payload": record.payload,
             "state": record.state,
             "author": record.author,
@@ -79,6 +119,7 @@ async def register(app: web.Application):
     app.add_routes(
         [
             web.post("/schema-exchange/send", send),
+            web.post("/schema-exchange/send-response", sendResponse),
             web.get("/schema-exchange/{hashid}", get),
         ]
     )
